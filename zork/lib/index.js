16a344b6dbf7f294b11985fecc30a3d493d8c08c
Elvin Yung <elvinyung1@gmail.com>
2015-07-09 00:43:59 -0700

    Add word ban list

--- zork/lib/index.js
+++ zork/lib/index.js
@@ -7,8 +7,16 @@ zork = new ZorkClient(function(data) {
   _channel && _channel.send(data);
 });
 
+var BANNED_WORDS = new Set(['quit']);
+
 module.exports = function zorkCmd(argv, response, channel) {
-  var cmd = argv.slice(1).join(' ');
+  var words = argv.slice(1);
+  for (var i in words) {
+    if BANNED_WORDS.has(words[i]) {
+      return;
+    }
+  }
+  var cmd = words.join(' ');
   _channel = channel;
   zork.send(cmd);
 };
