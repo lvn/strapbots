26a8598c546736bc4bfb8b02f756ab9f7ec12cf9
Elvin Yung <elvinyung1@gmail.com>
2015-07-29 21:46:14 -0700

    Use equality operator for checking multiplier

--- points/index.js
+++ points/index.js
@@ -3,7 +3,7 @@ var pointTable = {};
 
 module.exports = function(match, response) {
   var points = match[1],
-    multiplier = ('to' in match[2]) - ('from' in match[2]),
+    multiplier = ('to' === match[2]) - ('from' === match[2]),
     target = match[3];
 
 
