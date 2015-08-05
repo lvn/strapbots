5f54a6c59f4011b1a0027c9b92cbf532f4f457ad
Elvin Yung <elvinyung1@gmail.com>
2015-08-05 16:38:56 -0700

    Make point parsing and rendering less crappy

--- points/index.js
+++ points/index.js
@@ -1,12 +1,28 @@
 
+var math = require('mathjs');
+
 var pointTable = {};
 
-module.exports = function(match, response) {
-  var points = Number(match[1]),
-    multiplier = ('to' === match[2]) - ('from' === match[2]),
-    target = match[3];
+var renderPoints = function renderPoints(points) {
+  if (typeof points !== 'number' || points !== points) {
+    return 'wat';
+  }
 
+  if (points === Infinity) {
+    return 'way too many points';
+  }
+
+  return points.toString + ' points';
+};
+
+var points = function points(match, response) {
+  var matchExpr = match[1],
+    points = Math.round(math.eval(matchExpr)),
+    multiplier = ('to' === match[3]) - ('from' === match[3]),
+    target = match[4];
 
   pointTable[target] = (pointTable[target] || 0) + (points * multiplier);
-  response.end(target + ': ' + pointTable[target] + ' points');
-}
+  response.end(target + ': ' + renderPoints(pointTable[target]));
+};
+
+module.exports = points;
