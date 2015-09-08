08888335ac566286ddbfaa8e4ffe1b2ce59ac3f8
Elvin Yung <elvinyung1@gmail.com>
2015-09-08 14:19:47 -0700

    Track by channel id

--- combo-breaker/index.js
+++ combo-breaker/index.js
@@ -2,14 +2,34 @@
 
 var counts = {};
 
-var comboBreaker = function(message, response, logger) {
-  var text = message.text.trim().toLowerCase();
-  counts[text] = (counts[text] || 0) + 1;
-  logger.log(message, counts[text]);
-  if (counts[text] >= 2) {
-    logger.log('breaking combo', message);
-    response.end('C-C-C-COMBO BREAKER');
-    delete counts[text];
+var timeDiffSeconds = function timeDiff(a, b) {
+  a = a || Date.now();
+  b = b || Date.now();
+  return Math.abs(a - b) / 1000;
+};
+
+var comboBreaker = function(message, channel, response, logger) {
+  // get channel
+  var channelId = channel.id;
+  counts[channelId] = counts[channelId] || {};
+  var channelCounts = counts[channelId];
+
+  if (message.text) {
+    // normalize text
+    var text = message.text.trim().toLowerCase();
+    channelCounts[text] = channelCounts[text] || [];
+
+    // insert text time into record
+    var now = Date.now();
+    channelCounts[text].unshift(now);
+    logger.log(message, channelCounts[text].length);
+
+    if (timeDiffSeconds(channelCounts[text][0]) < 21600 &&
+        channelCounts[text].length >= 2) {
+      logger.log('breaking combo', message);
+      response.end('C-C-C-COMBO BREAKER');
+      delete counts[channelId][text];
+    }
   }
 };
 
