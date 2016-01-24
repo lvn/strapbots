f4b60f7ce9d507d7c5676b83bdbcfc6bcb5c995e
Elvin Yung <elvin@elvinyung.com>
2016-01-24 01:01:27 -0800

    Add anon plugin

---
+++ anon/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let anon = function anon(argv, channels, response, config, logger) {
+  let channelIdFmt = argv[1];
+  let msg = argv.slice(2).join(' ');
+  let match = /<#(.*)>/.exec(channelIdFmt);
+  let channelId = (match || [])[1];
+  let channel = channels[channelId];
+
+  if (channelId && msg && msg.length < 4000) {
+    response.channel = channel;
+    response.endf('{{anonName}} said: \n>>>{{msg}}', {
+      anonName: config.anonName,
+      msg: msg
+    });
+  }
+  else {
+    logger.error('Cannot find channel', channelId || 'with unknown ID');
+    response.end(config.errMsgs.noChannel);
+  }
+};
+
+anon.metadata = require('./plugin');
+
+module.exports = anon;
