251fa17f1376a72f19e9db682a1952fffc11b4c7
Elvin Yung <elvin@elvinyung.com>
2018-01-13 19:45:24 -0500

    Add reset capabilities

--- anon/index.js
+++ anon/index.js
@@ -10,42 +10,50 @@ let getAnonName = (userId) => {
 }
 
 let anon = function anon(argv, channels, response, config, logger, message, emojiService) {
-  let channelIdFmt = argv[1];
-  let msg = argv.slice(2).join(' ');
-  let match = /<#(\w*)(\|.*)?>/.exec(channelIdFmt);
-  let channelId = (match || [])[1];
-  let channel = channels[channelId];
+  let userId = message.sender.id;
+  let subcmd = argv[1];
+  if (subcmd == 'reset') {
+    Object.keys(usernames)
+      .filter((uname) => { uname.startsWith(userId) })
+      .forEach((uname) => { delete usernames[uname] });
+  } else {
+    let channelIdFmt = argv[1];
+    let msg = argv.slice(2).join(' ');
+    let match = /<#(\w*)(\|.*)?>/.exec(channelIdFmt);
+    let channelId = (match || [])[1];
+    let channel = channels[channelId];
 
-  if (channel && !channel.is_archived && msg && msg.length < 4000) {
+    if (channel && !channel.is_archived && msg && msg.length < 4000) {
 
-    if (!channel.is_member) {
-      response.endf('Thanks for invite to {{channel}}', {
-        channel: channelIdFmt
+      if (!channel.is_member) {
+        response.endf('Thanks for invite to {{channel}}', {
+          channel: channelIdFmt
+        });
+        return;
+      }
+
+      // assign anonName:
+      let userKey = `${userId}/${channelId}`;
+      if (!usernames[userKey]) {
+        var anonName = '';
+        do {
+          anonName = emojiService.random();
+        } while (usedNames.has(anonName));
+        usedNames.add(anonName);
+        usernames[userKey] = anonName;
+      }
+      var anonName = usernames[userKey];
+
+      response.channel = channel;
+      response.endf(':{{anonName}}: said: \n>>>{{msg}}', {
+        anonName: anonName,
+        msg: msg
       });
-      return;
     }
-
-    // assign anonName:
-    let userKey = `${message.channel.id}/${message.sender.id}`;
-    if (!usernames[userKey]) {
-      var anonName = '';
-      do {
-        anonName = emojiService.random();
-      } while (usedNames.has(anonName));
-      usedNames.add(anonName);
-      usernames[userKey] = anonName;
+    else {
+      logger.error('Cannot find channel', channelId || 'with unknown ID');
+      response.end(config.errMsgs.noChannel);
     }
-    var anonName = usernames[userKey];
-
-    response.channel = channel;
-    response.endf(':{{anonName}}: said: \n>>>{{msg}}', {
-      anonName: anonName,
-      msg: msg
-    });
-  }
-  else {
-    logger.error('Cannot find channel', channelId || 'with unknown ID');
-    response.end(config.errMsgs.noChannel);
   }
 };
 
