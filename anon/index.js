c67891be8920fdaf75df9408f51eb120ee01d506
Elvin Yung <elvin@elvinyung.com>
2018-01-13 19:32:34 -0500

    Emoji usernames

--- anon/index.js
+++ anon/index.js
@@ -1,6 +1,15 @@
 'use strict';
 
-let anon = function anon(argv, channels, response, config, logger) {
+let usernames = {};
+
+let usedNames = new Set();
+let getAnonName = (userId) => {
+  if (!usernames[userId]) {
+    usernames[userId] = getNextEmoji()
+  }
+}
+
+let anon = function anon(argv, channels, response, config, logger, message, emojiService) {
   let channelIdFmt = argv[1];
   let msg = argv.slice(2).join(' ');
   let match = /<#(\w*)(\|.*)?>/.exec(channelIdFmt);
@@ -16,9 +25,21 @@ let anon = function anon(argv, channels, response, config, logger) {
       return;
     }
 
+    // assign anonName:
+    let userKey = `${message.channel.id}/${message.sender.id}`;
+    if (!usernames[userKey]) {
+      var anonName = '';
+      do {
+        anonName = emojiService.random();
+      } while (usedNames.has(anonName));
+      usedNames.add(anonName);
+      usernames[userKey] = anonName;
+    }
+    var anonName = usernames[userKey];
+
     response.channel = channel;
-    response.endf('{{anonName}} said: \n>>>{{msg}}', {
-      anonName: config.anonName,
+    response.endf(':{{anonName}}: said: \n>>>{{msg}}', {
+      anonName: anonName,
       msg: msg
     });
   }
